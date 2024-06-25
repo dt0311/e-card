2b87cc17b4b5f4eec35f66656432d0471836d318
Divyatyagi-hub <tyagidivya68@gmail.com>
2024-06-25 11:15:26 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Example JavaScript code
+document.getElementById('D:/Arani2/Background E-Card.png').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default action of the link (if href="#" is used)
+    // Add your custom functionality here, like changing the image source
+    // Example: document.querySelector('img').src = 'newimage.jpg';
+});
